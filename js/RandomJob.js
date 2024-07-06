12269ca9f4ea50774a0a348e68570b945b0caa6b
Daniel Schurmann <13761703+DSchurmann@users.noreply.github.com>
2024-07-06 15:18:25 +1000

    7.0 update
    -Added viper and pictomancer

--- js/RandomJob.js
+++ js/RandomJob.js
@@ -13,12 +13,14 @@ const JOBLIST = [
 	"Ninja",
 	"Samurai",
 	"Reaper",
+	"Viper",
 	"Bard",
 	"Machinist",
 	"Dancer",
 	"Black Mage",
 	"Summoner",
-	"Red Mage"
+	"Red Mage",
+	"Pictomancer"
 	];
 
 function loadImages() {
@@ -39,72 +41,63 @@ function generate() {
 }
 
 function getSelectedJobs() {
-	tanks = [];
-	healers = [];
-	mdps = [];
-	rdps = [];
-	madps = [];
 	jobs = [];
 
 	if(document.getElementById("tanks").checked == true) {
 		if(document.getElementById("pal").checked == true)
-			tanks.push("Paladin");
+			jobs.push("Paladin");
 		if(document.getElementById("war").checked == true)
-			tanks.push("Warrior");
+			jobs.push("Warrior");
 		if(document.getElementById("drk").checked == true)
-			tanks.push("Dark Knight");
+			jobs.push("Dark Knight");
 		if(document.getElementById("gnb").checked == true)
-			tanks.push("Gunbreaker");
+			jobs.push("Gunbreaker");
 	}
 
 	if(document.getElementById("healers").checked == true) {
 		if(document.getElementById("whm").checked == true)
-			tanks.push("White Mage");
+			jobs.push("White Mage");
 		if(document.getElementById("sch").checked == true)
-			tanks.push("Scholar");
+			jobs.push("Scholar");
 		if(document.getElementById("ast").checked == true)
-			tanks.push("Astrologian");
+			jobs.push("Astrologian");
 		if(document.getElementById("sge").checked == true)
-			tanks.push("Sage");
+			jobs.push("Sage");
 	}
 
 	if(document.getElementById("mdps").checked == true) {
 		if(document.getElementById("mnk").checked == true)
-			tanks.push("Monk");
+			jobs.push("Monk");
 		if(document.getElementById("drg").checked == true)
-			tanks.push("Dragoon");
+			jobs.push("Dragoon");
 		if(document.getElementById("nin").checked == true)
-			tanks.push("Ninja");
+			jobs.push("Ninja");
 		if(document.getElementById("sam").checked == true)
-			tanks.push("Samurai");
+			jobs.push("Samurai");
 		if(document.getElementById("rpr").checked == true)
-			tanks.push("Reaper");
+			jobs.push("Reaper");
 	}
 
 	if(document.getElementById("rdps").checked == true) {
 		if(document.getElementById("brd").checked == true)
-			tanks.push("Bard");
+			jobs.push("Bard");
 		if(document.getElementById("mch").checked == true)
-			tanks.push("Machinist");
+			jobs.push("Machinist");
 		if(document.getElementById("dnc").checked == true)
-			tanks.push("Dancer");
+			jobs.push("Dancer");
 	}
 
 	if(document.getElementById("madps").checked == true) {
 		if(document.getElementById("blm").checked == true)
-			tanks.push("Black Mage");
+			jobs.push("Black Mage");
 		if(document.getElementById("smn").checked == true)
-			tanks.push("Summoner");
+			jobs.push("Summoner");
 		if(document.getElementById("rdm").checked == true)
-			tanks.push("Red Mage");
+			jobs.push("Red Mage");
+		if(document.getElementById("rdm").checked == true)
+			jobs.push("Pictomancer");
 	}
 
-
-	jobs = jobs.concat(tanks);
-	jobs = jobs.concat(healers);
-	jobs = jobs.concat(mdps);
-	jobs = jobs.concat(rdps);
-	jobs = jobs.concat(madps);
 	return jobs;
 }
 
